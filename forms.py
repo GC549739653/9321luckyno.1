f99b4b491a35f19b12286e4b3211819d217d2e69
Roy <358963981@qq.com>
2019-04-13 13:53:42 +1000

    Update on backend

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms import *
+from wtforms.validators import *
 
 
 class RegistrationForm(FlaskForm):
@@ -20,3 +20,28 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class PredictForm(FlaskForm):
+    age = IntegerField("Age", validators=[NumberRange(min=1,max=100)])
+    sex = SelectField("Sex", choices=[(1,"Male"),(0,"Female")],coerce=int)
+    cp = SelectField("Chest Pain Type", choices=[(1,"Typical angin"),
+        (2,"atypical angina"),
+        (3,"non-anginal pain"),
+        (4,"asymptomatic")],coerce=int)
+    trestbps = IntegerField("Resting Blood Pressure", validators=[DataRequired(),NumberRange(min=80,max=200)])
+    chol = IntegerField("Serum Cholestoral in mg/dl", validators=[DataRequired(),NumberRange(min=100,max=600)])
+    fbs = SelectField("Fasting Blood Sugar > 120 mg/dl?",choices=[(1,"Yes"),
+        (0,"No")],coerce=int)
+    restecg = SelectField("Resting Electrocardiographic Result",choices=[(0,"Normal"),
+        (1,"Having ST-T Wave Abnormality"),
+        (2,"Showing Probable or Definite Left Ventricular Hypertrophy")],coerce=int)
+    thalach = IntegerField("Maximum Heart Rate Achieved", validators=[DataRequired(),NumberRange(min=50,max=250)])
+    exang = SelectField("Exercise Induced Angina", choices=[(1,"Yes"),
+        (0,"No")],coerce=int)
+    oldpeak = DecimalField("ST Depression Indec. Ex.", validators=[DataRequired()])
+    slope = IntegerField("Slope of Peak Exercise ST", validators=[DataRequired()])
+    ca = SelectField("Number of Major Vessel", choices=[(0,"0"),
+        (1,"1"),
+        (2,"2"),
+        (3,"3")],coerce=int)
+    submit = SubmitField("Submit")
